3ed43769fa5d2d66048f3edb5d1d2b2c4ee7683a
atikadewisetiani <95559118+atikasetiani@users.noreply.github.com>
2023-06-23 13:47:52 +0700

    Update web testing terbaru

---
+++ src/test/java/starter/stepDefinitions/AddCourseSteps.java
@@ -0,0 +1,54 @@
+package starter.stepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.thucydides.core.annotations.Steps;
+import starter.StarEdu.Home;
+
+public class AddCourseSteps {
+    @Steps
+    Home home;
+
+    @Given("I am on the homepage")
+    public void IamOnHomePage(){
+        home.openUrl("https://staredu-desktop-dev.netlify.app/dashboard");
+        home.iamOnTheHomePage();
+    }
+
+    @When("I click on the Add Course button")
+    public void ClickAddCouseSection(){
+        home.clickAddCourseSection();
+    }
+
+    @And("I Fill in the Course Name field {string}")
+    public void FillNameCourse(String NameCourse){
+        home.fillNameCourse(NameCourse);
+    }
+
+    @And("I fill in the Live Session Schedule field {string}")
+    public void FillLiveSessionSchedule(String LiveSessionSchedule){
+        home.fillLiveSessionSchedule(LiveSessionSchedule);
+    }
+
+    @And("I click tumbnail logo")
+    public void ClickTumbnailButton(){
+        home.clickTumbnailButton();
+    }
+
+    @And("I Choose the thumbnail")
+    public void ChooseTumbnailIcon(){
+        home.clickTumbnailIcon();
+    }
+
+    @And("I Click on the Add Course button")
+    public void ClickAddCourseButton(){
+        home.clickAddCourse();
+    }
+
+    @Then("I success add course")
+    public void SuccessAddCourse(){
+        home.validateOnTheHomePage();
+    }
+}

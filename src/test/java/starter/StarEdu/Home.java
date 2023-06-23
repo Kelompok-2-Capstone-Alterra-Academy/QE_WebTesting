3142796b778e0e570076efea98db6df5298fe902
atikadewisetiani <95559118+atikasetiani@users.noreply.github.com>
2023-06-23 09:48:42 +0700

    Add Web Testing

---
+++ src/test/java/starter/StarEdu/Home.java
@@ -0,0 +1,67 @@
+package starter.StarEdu;
+
+import net.serenitybdd.screenplay.actions.OpenUrl;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+
+public class Home extends PageObject {
+    private By dashboard(){
+        return By.xpath("//*[@id=\"root\"]");
+    }
+    private By AddCourseSection() { return By.xpath("//*[@id=\"root\"]/div[1]/div[2]/div/div/div[3]/div[2]/div[1]");}
+    private By nameCourseField() {return By.xpath("/html/body/div[2]/div[3]/div[2]/div[1]/input");}
+    private By liveSessionScheduleField() {return By.xpath("/html/body/div[2]/div[3]/div[2]/div[2]/input");}
+    private By tumbnailButton(){return By.xpath("/html/body/div[2]/div[3]/div[2]/div[3]/div");}
+    private By tumbnailIcon() {return By.xpath("/html/body/div[2]/div[3]/div[2]/div[3]/div/div/img[2]");}
+    private By addCourseButton() {return By.xpath("/html/body/div[2]/div[3]/div[2]/div[4]/button[2]");}
+
+    @Step
+    public static OpenUrl url(String targetUrl){
+        return new OpenUrl(targetUrl);
+    }
+
+    @Step
+    public void iamOnTheHomePage() {$(AddCourseSection()).isDisplayed();
+    }
+    @Step
+    public void validateOnTheHomePage(){
+        $(dashboard()).isDisplayed();
+    }
+
+    @Step
+    public void clickAddCourseSection(){
+        $(AddCourseSection()).click();
+    }
+
+    @Step
+    public void fillNameCourse(String NameCourse){
+        $(nameCourseField()).type(NameCourse);
+    }
+
+    @Step
+    public void fillLiveSessionSchedule(String LiveSessionSchedule){
+        $(liveSessionScheduleField()).type(LiveSessionSchedule);
+    }
+
+    @Step
+    public void clickTumbnailButton(){
+        $(tumbnailButton()).click();
+    }
+
+    @Step
+    public void clickTumbnailIcon(){
+        $(tumbnailIcon()).click();
+    }
+
+    @Step
+    public void clickAddCourse(){
+        $(addCourseButton()).click();
+    }
+
+    @Step
+    public void successAddCouse(){
+        $(addCourseButton()).isDisplayed();
+    }
+
+}
